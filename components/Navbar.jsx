5df22b24ff13dfd7c6bc81159373115da753144a
oladayo Adekoya <adekoyadayo2010@gmail.com>
2024-06-25 22:11:56 +0100

    change link names

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -2,15 +2,15 @@ const Navbar = () => {
   return (
     <nav className="w-full bg-red-200 h-24 flex justify-center">
       <div className="container bg-slate-100 p-3 flex items-center gap-x-3">
-        <div id="logo" className="bg-slate-500 basis-1/3">
-          logo
+        <div id="logo" className="bg-slate-500 basis-1/4 text-3xl">
+          Nethub
         </div>
-        <ul className="flex bg-slate-500 basis-2/3">
-          <li className="px-4 py-2">list</li>
-          <li className="px-4 py-2">list</li>
-          <li className="px-4 py-2">list</li>
-          <li className="px-4 py-2">list</li>
-          <li className="px-4 py-2">list</li>
+        <ul className="flex bg-slate-500 basis-3/4 justify-end">
+          <li className="px-4 py-2">About</li>
+          <li className="px-4 py-2">Solutions</li>
+          <li className="px-4 py-2">Resources</li>
+          <li className="px-4 py-2">Contact Us</li>
+          <li className="px-4 py-2">Search</li>
         </ul>
       </div>
     </nav>
