d616928f1d4969481b19e866955363c877d5b43f
oladayo Adekoya <adekoyadayo2010@gmail.com>
2024-06-25 10:23:56 +0100

    add navbar and footer cmpt to layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Navbar from "../../components/Navbar";
+import Footer from "../../components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,7 +13,14 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <main className="bg-red-300 min-h-screen">
+          <Navbar />
+          {children}
+          <Footer />
+          <div className="w-full max-w-[1140px] bg-slate-700 my-0 mx-auto"></div>
+        </main>
+      </body>
     </html>
   );
 }
