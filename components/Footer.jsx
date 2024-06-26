4bde2958f54b4e0dffae6a0c9d03ff01073b032e
oladayo Adekoya <adekoyadayo2010@gmail.com>
2024-06-26 10:37:59 +0100

    footer contact me

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,11 +1,21 @@
 const Footer = () => {
   return (
     <div id="footer">
-      <div id="contact" className="h-[674px]">
-        <div className="container h-5 bg-customColors-fiord">talk true</div>
+      <div
+        id="contact"
+        className="h-[674px] bg-customColors-fiord flex justify-center pt-[70px]"
+      >
+        <div className="container h-full bg-red-300 mx-4 flex flex-wrap flex-initial">
+          <div className="bg-slate-200 basis-full md:basis-2/4 flex justify-center flex-initial">
+            forms
+          </div>
+          <div className="bg-slate-200 basis-full md:basis-2/4 flex justify-center flex-initial">
+            contacts
+          </div>
+        </div>
       </div>
       <br />
-      <div className="h-[674px]"></div>
+      <div className="h-[674px] bg-customColors-fiord"></div>
     </div>
   );
 };
