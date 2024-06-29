42b8d8126d4f3821b7850fe99567927b7c1f4e30
oladayo Adekoya <adekoyadayo2010@gmail.com>
2024-06-29 17:29:55 +0100

    create logo and add to footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -4,7 +4,13 @@ import { ContactForm } from "./Contactform";
 import { CommunityIcons, NewsletterIcons } from "../assets/icons";
 import ImageBox from "./imageBox";
 import profilePic from "../assets/canada.webp";
-import { FaAngleRight } from "react-icons/fa";
+import Logo from "./Logo";
+import {
+  FaAngleRight,
+  FaCookieBite,
+  FaLock,
+  FaRegHandPointRight,
+} from "react-icons/fa";
 
 const Footer = () => {
   return (
@@ -229,30 +235,43 @@ const Footer = () => {
           </div>
 
           <div id="privacy-policy" className="h-[275.20px] pt-16">
-            <div className="bg-red-300 h-full container flex flex-col items-center">
-              <h2 className="p-2.5 mb-9">Helping tacos</h2>
-              <p className="mb-5">coming soon</p>
-              <div className="flex justify-center gap-3 pb-5 ">
-                <p className="bg-slate-300">
-                  <span>icon</span> cookie policy
+            <div className="h-full container flex flex-col items-center">
+              <h2 className="p-2.5 mb-9 text-2xl text-primary">
+                Helping telcos connect people since 2013
+              </h2>
+              <p className="mb-8 text-sm text-customColors-loblolly/85">
+                © 2001 – 2024 PortaOne Inc. All rights reserved.
+              </p>
+              <div className="flex justify-center gap-5 pb-5 text-customColors-loblolly">
+                <p className="flex gap-2 capitalize text-sm items-center">
+                  <span className="text-xl text-customColors-fiord">
+                    <FaCookieBite />
+                  </span>{" "}
+                  cookie policy
                 </p>
-                <p className="bg-slate-300">
-                  <span>icon</span> terms of use
+                <p className="flex gap-2 capitalize text-sm items-center">
+                  <span className="text-xl text-customColors-fiord">
+                    <FaRegHandPointRight />
+                  </span>{" "}
+                  terms of use
                 </p>
-                <p className="bg-slate-300">
-                  <span>icon</span> privacy policy
+                <p className="flex gap-2 capitalize text-sm items-center">
+                  <span className="text-xl text-customColors-fiord">
+                    <FaLock />
+                  </span>{" "}
+                  privacy policy
                 </p>
               </div>
             </div>
           </div>
 
           <div id="bottom footer" className="h-[291px] pt-5 pb-20">
-            <div className="bg-red-300 h-full container flex flex-wrap justify-center items-center">
+            <div className="h-full container flex flex-wrap justify-center items-center">
               {/* logo */}
-              <div className="bottomftr flex justify-end">logo Nethub</div>
+              <Logo />
               {/* address */}
-              <div className="bottomftr">
-                <p>Nethub Tech</p>
+              <div className="bottomftr text-customColors-loblolly/85">
+                <p className="font-semibold">Nethub Tech</p>
                 <p>A113 - 2009 lougheed hwy</p>
                 <p>port coloquaim, BV VBC Abuja, Nigeria</p>
               </div>
---
+++ components/Logo.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { FaThLarge } from "react-icons/fa";
+
+const Logo = () => {
+  return (
+    <div className="bottomftr flex justify-end items-center gap-2 text-white text-3xl font-bold">
+      <span className="text-2xl text-white bg-primary h-11 w-11 flex justify-center items-center rounded-full">
+        <FaThLarge />
+      </span>{" "}
+      Nethub
+    </div>
+  );
+};
+
+export default Logo;
