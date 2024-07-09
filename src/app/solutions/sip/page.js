9c702f00bb2aa5c4d57cd36c68c0f9f9a5808661
oladayo Adekoya <adekoyadayo2010@gmail.com>
2024-07-10 00:12:15 +0100

    add whats in it and why choose nethub sections

--- src/app/solutions/sip/page.js
+++ src/app/solutions/sip/page.js
@@ -1,10 +1,11 @@
 import React from "react";
 import Breadcrumbs from "../../../../components/Breadcrumbs.jsx";
 import PageHeader from "../../../../components/PageHeader.jsx";
-import { MdDialerSip } from "react-icons/md";
 import BizPerksCard from "../../../../components/BizPerksCard.jsx";
-import { Hsas, Hc, Sds, Rnr } from "../../../../components/icons";
-import { FiPhoneCall } from "react-icons/fi";
+import { Button } from "@/components/ui/button.jsx";
+import { disBstWay, whatsinit } from "@/lib/data/sipdata.jsx";
+import { FaChevronRight } from "react-icons/fa6";
+import Link from "next/link.js";
 
 const Sip = () => {
   // SIP solutions page breadcrumbs
@@ -28,9 +29,162 @@ const Sip = () => {
         height=""
       />
       {/* SIP solutions content */}
-      <section className="bg-white pt-10 pb-10 ">
+      <section className="bg-customColors-porcelain pt-10 pb-10 min-h-96">
         <div className="container">
           <Breadcrumbs crumbs={crumbs} />
+          {/* profitable sip*/}
+          <div className="my-10 grid sm:grid-cols-1 md:grid-cols-2 gap-5">
+            <div className="p-[30px]">
+              <p className="text-4xl text-primary font-semibold mb-8">
+                Your SIP trunking services can be more profitable
+              </p>
+              <p className="text-customColors-dovegray text-lg mb-4">
+                As a SIP trunking provider, you are constantly fighting tight
+                margins, high churn, and revenue leakage. Margins on your core
+                services are low, hungry new competitors are always pitching a
+                lower price, and your enterprise customers are demanding the
+                latest PBX features before you’re ready to offer them. Here’s
+                the good news: there is a clear pathway to profitability!
+              </p>
+              <p className="text-customColors-dovegray text-lg">
+                NetSwitch is a customizable, scalable PBX/UCaaS platform that
+                can help you design targeted, tailored packages for the
+                businesses and call centers within your specific market. More
+                importantly,{" "}
+                <span className="font-semibold">
+                  since Nethub does not offer VoIP connectivity and resells no
+                  SIP trunks,
+                </span>{" "}
+                you can deal with any carriers and termination partners of your
+                choice, and get the best bargains you can find on your own. And
+                with our flexible pricing models, 7-week software update
+                schedule, and open API, you can scale and adapt as quickly as
+                you need to.
+              </p>
+            </div>
+            {/* review */}
+            <div className="rounded-[50px] mt-5 mx-8 mb-8 p-10 bg-white">
+              <div>
+                <p className="text-customColors-fiord text-2xl font-semibold">
+                  Michelle Igwe
+                </p>
+                <p className="text-primary text-lg font-medium mt-1 mb-5">
+                  Marketing Manager at Nethub
+                </p>
+              </div>
+              <p className="italic text-[22px] text-customColors-dovegray mb-[33px]">
+                Over The Top (OTT) communication has rapidly become one of the
+                most popular ways for end users to send and receive audio,
+                voice, share files, and collaborate – all while using the
+                Internet.
+              </p>
+              <p className="italic text-[22px] text-customColors-dovegray">
+                The explosive demand for OTT services and software can mean big
+                opportunities for UCaaS providers.
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+      {/* Discover a better way */}
+      <section className="py-16">
+        <div className="container">
+          <h3 className="text-5xl text-primary font-semibold mb-8">
+            Discover a better way
+          </h3>
+          <p className="text-3xl tracking-wide text-customColors-dovegray">
+            If you’re ready to change all of that, then Nethub is here to help.
+            We can offer a software solution for your SIP trunking business that
+            will:
+          </p>
+          <div className="grid grid-cols-1 md:grid-cols-5 gap-5 lg:gap-8 my-16">
+            {disBstWay.map(({ icon, title, content }) => {
+              return (
+                <BizPerksCard
+                  key={title}
+                  title={title}
+                  content={content}
+                  icon={icon}
+                />
+              );
+            })}
+          </div>
+        </div>
+      </section>
+      {/* whats in it for you */}
+      <section className="py-16 bg-customColors-porcelain">
+        <div className="container">
+          <h3 className="text-5xl text-primary font-semibold mb-8">
+            What&apos;s in it for you?
+          </h3>
+          <p className="text-xl font-medium tracking-wide">
+            See why SIP trunking providers are choosing NetSwitch to provide
+            enterprise PBX, call center connectivity, and more.
+          </p>
+          <div className="grid grid-cols-1 md:grid-cols-5 gap-5 lg:gap-8 my-16">
+            {whatsinit.map(({ icon, title, content }) => {
+              return (
+                <BizPerksCard
+                  key={title}
+                  title={title}
+                  content={content}
+                  icon={icon}
+                />
+              );
+            })}
+          </div>
+          <p className="text-lg">
+            Ready to talk? Let’s arrange a demo via a personal call or online
+            presentation.
+          </p>
+          <Button className="mt-4 uppercase font-semibold">
+            <Link href="#footer">
+              <p className="flex items-center gap-2">
+                contact us
+                <span className="text-xs">
+                  <FaChevronRight />
+                </span>
+              </p>
+            </Link>
+          </Button>
+        </div>
+      </section>
+      {/* why choose Nethub */}
+      <section className="py-16 bg-customColors-fiord">
+        <div className="container">
+          <h3 className="text-5xl text-primary font-semibold mb-8">
+            What&apos;s in it for you?
+          </h3>
+          <p className="text-xl font-medium tracking-wide">
+            See why SIP trunking providers are choosing NetSwitch to provide
+            enterprise PBX, call center connectivity, and more.
+          </p>
+          <div className="grid grid-cols-1 md:grid-cols-5 gap-5 lg:gap-8 my-16">
+            {whatsinit.map(({ icon, title, content }) => {
+              return (
+                <BizPerksCard
+                  key={title}
+                  title={title}
+                  content={content}
+                  icon={icon}
+                />
+              );
+            })}
+          </div>
+          <p className="text-lg">
+            Ready to talk? Let’s arrange a demo via a personal call or online
+            presentation.
+          </p>
+          <Button className="mt-4 uppercase font-semibold">
+            <Link href="#footer">
+              <p className="flex items-center gap-2">
+                contact us
+                <span className="text-xs">
+                  <FaChevronRight />
+                </span>
+              </p>
+            </Link>
+          </Button>
         </div>
       </section>
     </div>
---
+++ src/lib/data/sipdata.jsx
@@ -0,0 +1,77 @@
+import { Hsas, Hc, Sds, Rnr } from "../../../components/icons";
+
+export const disBstWay = [
+  {
+    title: "Grow your revenue and profitability",
+    icon: <Sds fill="#9EAEB4" />,
+    content:
+      "with appealing VAS add-ons that will attract more customers and boost profitability",
+    bgcolor: "",
+  },
+  {
+    title: "Improve your margins",
+    icon: <Hsas fill="#9EAEB4" />,
+    content:
+      "and decrease termination costs by enabling sophisticated routing policies",
+    bgcolor: "",
+  },
+  {
+    title: "Save time and energy",
+    icon: <Hc fill="#9EAEB4" />,
+    content:
+      "by reducing manual configuration, automating on-boarding and simplifying troubleshooting",
+    bgcolor: "",
+  },
+  {
+    title: "Lower operating expenses",
+    icon: <Sds fill="#9EAEB4" />,
+    content:
+      "by providing an integrated billing platform included with PortaSwitch at no extra cost",
+    bgcolor: "",
+  },
+  {
+    title: "Help upsell",
+    icon: <Rnr fill="#9EAEB4" />,
+    content:
+      "your customers cloud PBX service using the same platform to increase profit margins and customer retention",
+    bgcolor: "",
+  },
+];
+
+export const whatsinit = [
+  {
+    title: "Differentiate to gain",
+    icon: <Sds fill="#9EAEB4" />,
+    content:
+      "Differentiate yourself, gain more customers, decrease churn with value added services via Add-on Mart - an iPaaS marketplace",
+    bgcolor: "",
+  },
+  {
+    title: "Watch savings increase",
+    icon: <Hsas fill="#9EAEB4" />,
+    content:
+      "With reduced manual work, a great deal: NetSIP is included in NetSwitch",
+    bgcolor: "",
+  },
+  {
+    title: "Prevent fraud and control spending",
+    icon: <Hc fill="#9EAEB4" />,
+    content:
+      "Control and monitor credit limits in real time, block calls from suspicious locations, check customer activity vs regular usage patterns",
+    bgcolor: "",
+  },
+  {
+    title: "Monetize fast!",
+    icon: <Sds fill="#9EAEB4" />,
+    content:
+      "Speed up the process to monetization by going to market faster with our 7 week agile development cycle",
+    bgcolor: "",
+  },
+  {
+    title: "Enjoy the freedom",
+    icon: <Rnr fill="#9EAEB4" />,
+    content:
+      "That confidence and reliability grant you, knowing that our ‘best-in-industry’ technical support team is there 24/7",
+    bgcolor: "",
+  },
+];
