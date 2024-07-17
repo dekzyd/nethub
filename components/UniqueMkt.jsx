a5714a59dc3f2c09ac6b749107c4543565d9282e
oladayo Adekoya <adekoyadayo2010@gmail.com>
2024-07-17 23:02:24 +0100

    update uniquemkt title and layout

--- components/UniqueMkt.jsx
+++ components/UniqueMkt.jsx
@@ -7,14 +7,17 @@ const UniqueMkt = () => {
   return (
     <section className="bg-customColors-porcelain py-16">
       <div className="container">
-        <div className="py-5 max-w-[687px]">
-          <p className="uppercase font-medium text-xl tracking-widest pb-3">
-            what makes Nethub unique
-          </p>
-          <p className="text-primary text-3xl font-semibold tracking-wide">
-            Here’s why telcos around the world choose our products and services
-            to help them succeed
-          </p>
+        <div className="grid grid-cols-1 lg:grid-cols-2">
+          <div className="p-5 max-w-[687px]">
+            <p className="uppercase font-medium text-xl tracking-widest pb-3">
+              what makes Nethub unique
+            </p>
+            <p className="text-primary text-[32px] font-semibold tracking-wide">
+              Here’s why telcos around the world choose our products and
+              services to help them succeed
+            </p>
+          </div>
+          <div className="hidden lg:grid"></div>
         </div>
         {/* biz perks cards */}
         <div className="grid sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-5 py-5">
