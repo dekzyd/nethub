6f2bb2b173401ebf8115b581fce533ec04259328
oladayo Adekoya <adekoyadayo2010@gmail.com>
2024-07-21 22:44:07 +0100

    update mission cmpt styles and layout

--- components/Mission.jsx
+++ components/Mission.jsx
@@ -4,27 +4,27 @@ import { FaStar } from "react-icons/fa6";
 const Mission = () => {
   return (
     <div className="bg-customColors-porcelain h-[666px] pt-12 pb-14">
-      <div className="bg-red-20 container">
+      <div className="container h-full flex flex-col justify-around">
         <div className="grid grid-cols-1 md:grid-cols-2 gap-x-9">
-          <div className="py-5">
-            <p className="pre-title mb-5">the nethub mission</p>
-            <h2 className="text-primary text-6xl font-semibold tracking-normal leading-snug">
+          <div className="p-5">
+            <p className="pre-title mb-7">the nethub mission</p>
+            <h2 className="text-primary text-6xl font-semibold -tracking-[0.8px] leading-[70.2px]">
               Help telcos connect people
             </h2>
           </div>
           <div className="pt-5">
-            <p className="text-customColors-fiord pb-5 text-3xl">
+            <p className="text-customColors-fiord pb-6 text-[33px] tracking-wide leading-[40.5px]">
               We empower the telecom community to provide reliable and
               easy-to-use services to everyone.
             </p>
-            <p className="text-customColors-dune text-lg font-normal">
+            <p className="text-customColors-dune text-xl tracking-normal leading-[31.5px] font-normal ">
               Nethub supplies integrated and flexible BSS, OCS, and softswitch
               solutions for telcos to help them easily compete and succeed.
             </p>
           </div>
         </div>
 
-        <div className=" mt-20">
+        <div className="">
           <p className="text-center py-5 text-customColors-hitgray text-2xl">
             Trusted by telecommunication service providers around the globe.
           </p>
